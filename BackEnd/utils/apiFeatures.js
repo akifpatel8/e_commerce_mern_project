d657b3ec954912ec6f8ef5468f7f5e4e2f37a36e
akifpatel8 <aakifpatel08@gmail.com>
2024-01-15 18:47:55 +0530

    be filter and pagination implemented

--- BackEnd/utils/apiFeatures.js
+++ BackEnd/utils/apiFeatures.js
@@ -18,6 +18,22 @@ class ApiFeatures{
         this.query = this.query.find({...keyword})
         return this
     }
+    filter(){
+        const queryCopy={...this.queryStr}
+        const removeItems=["keyword","page","limit"]
+        removeItems.forEach((el)=>delete queryCopy[el])
+        //filter for price and rating
+        let queryStr=JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g,(key)=>`$${key}`)
+        this.query=this.query.find(JSON.parse(queryStr))
+        return this
+    }
+    pagination(resultPerPage){
+        const currentPage = Number(this.queryStr.page) || 1
+        const skip = resultPerPage * (currentPage-1)
+        this.query=this.query.limit(resultPerPage).skip(skip)
+        return this  
+    }
 }
 
 module.exports = ApiFeatures 
